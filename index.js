49975502b5e5ca6640cba01a4cf36d3a3e6e6543
Fukki <takungwong@gmail.com>
2018-12-15 13:51:22 +0700

    :/

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 module.exports = function RequestLogs(mod) {
 	const cmd = mod.command || mod.require.command;
+	
 	mod.hook('S_REQUEST_CONTRACT', 1, e => {
 		let sender = e.senderName;
 		switch (e.type) {
 			case 3:
 				msglogs(sender + ' sent you a trade request at');
 				break;
-			case 4:
-				msglogs(sender + ' sent you a party invite at');
-				break;
-			case 5:
-				msglogs(sender + ' sent you a party joined at');
-				break;
 			case 11:
 				msglogs(sender + ' sent you a duel request at');
 				break;
 			case 18:
 				msglogs(sender + ' sent you a deathmatch invite at');
+				break;
+		}
+	});
+	
+	mod.hook('S_BEGIN_THROUGH_ARBITER_CONTRACT', 1, e => {
+		let sender = e.sender;
+		switch (e.type) {
+			case 4:
+				msglogs(sender + ' sent you a party invite at');
+				break;
+			case 5:
+				msglogs(sender + ' sent you a party joined at');
 				break;
 		}
 	});
